bbe0fff332bd05a22fb8473569bca9639042e662
unknown <carlosfl1296@gmail.com>
2025-10-29 13:45:12 +0100

    ruta a admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,11 @@ export const routes: Routes = [
     { path: '', redirectTo: 'transfer', pathMatch: 'full' },
     { path: 'transfer', loadComponent: () => import('./features/transfer/sender/sender.page').then(m => m.SenderPage) },
     { path: 'display', loadComponent: () => import('./features/transfer/receiver/receiver.page').then(m => m.ReceiverPage) },
+    {
+        path: 'admin',
+        loadComponent: () =>
+            import('./features/admin/admin.page/admin.page').then(m => m.AdminPage),
+    },
+
     { path: '**', redirectTo: 'transfer' }
 ];
